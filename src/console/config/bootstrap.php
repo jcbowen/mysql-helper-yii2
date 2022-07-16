18674802d5e0918a7588bac7fcb8b7f91a25b78c
jcbowen <bowen@jiuchet.com>
2022-07-16 15:44:13 +0800

    移除 控制台目录

--- src/console/config/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('IN_JC') or exit('Access Denied');
-
-Yii::setAlias('@console', dirname(__DIR__, 2) . '/console');
-
-const NO_TIME = '0000-00-00 00:00:00';
--- src/console/config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('IN_JC') or exit('Access Denied');
-
-return [
-    'adminEmail' => 'admin@example.com',
-];
